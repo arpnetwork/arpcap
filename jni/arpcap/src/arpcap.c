083265f8b402615f11d8c503117e6da62c824de6
rdspot <rdspot@arpnetwork.org>
2018-09-08 15:56:10 +0800

    Remove verbose short option

--- jni/arpcap/src/arpcap.c
+++ jni/arpcap/src/arpcap.c
@@ -72,7 +72,7 @@ int main(int argc, char *argv[])
   {
     int option_index = 0;
 
-    c = getopt_long(argc, argv, "b:c:s:r:pv", long_options, &option_index);
+    c = getopt_long(argc, argv, "b:c:s:r:p", long_options, &option_index);
     if (c == -1)
     {
       break;
